a0a1d0704e5fd2ca95aae6147428387499eff596
Vcoch27 <hoangtungmy123@gmail.com>
2025-07-31 01:23:30 +0700

    chore(frontend): minor change test CI/CD

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Hello, Tea Oil Project 👋</h1>
+      <h1>Hello, Tea Oil Project  hẹ hẹ hẹ👋</h1>
     </main>
   );
 }
